7b39c617ed057e0d72d0e779ec85355e29b7e0bb
Julliet Villalobos <jvillalobos@talana.com>
2019-11-22 14:07:31 -0300

    Updates

---
+++ precache-manifest.93abfe7b58cb37fff43e65101d79da72.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b89b84833259090004eb1f55e6518f",
+    "url": "/reax/index.html"
+  },
+  {
+    "revision": "8ae98f3372cb82fb8cef",
+    "url": "/reax/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "160baa9b8fc0ef3842c8",
+    "url": "/reax/static/css/main.b8f94fc3.chunk.css"
+  },
+  {
+    "revision": "8ae98f3372cb82fb8cef",
+    "url": "/reax/static/js/2.3a6c8352.chunk.js"
+  },
+  {
+    "revision": "160baa9b8fc0ef3842c8",
+    "url": "/reax/static/js/main.67349027.chunk.js"
+  },
+  {
+    "revision": "ff99489a1ae51f001934",
+    "url": "/reax/static/js/runtime-main.4d9a8078.js"
+  },
+  {
+    "revision": "d71843a9a289e4c1f07f682109c8226d",
+    "url": "/reax/static/media/mountain.d71843a9.jpg"
+  },
+  {
+    "revision": "e742ffb7dbc05d05850589ee76812f63",
+    "url": "/reax/static/media/woman.e742ffb7.jpg"
+  }
+]);
